6c2e6246c9226fba721290c4fde7e4760dd99483
Daniel <daniel.correa@autodesk.com>
2017-12-14 17:25:43 -0500

    Fetching data :)

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const apiKey = '6dc0d2605088c01254ffedbd444bc2e4';
 
 const getMovies = () => {
-    return fetch(`https://api.themoviedb.org/3/movie/latest?api_key=${apiKey}&language=en-US
+    return fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US
     `).then(movies => {
         return movies.json();
     });
 };
 
 let currentMovies = null;
+const listContainer = document.getElementById('movies-list');
+
 getMovies().then(response => {
-    console.log(response);
-    currentMovies = response;
+    
+    currentMovies = response.results;
+    currentMovies.map(element => element.title).forEach(element => {
+        const currentLi = document.createElement('li');
+        currentLi.className = 'movie-title';
+        const currentLiText = document.createTextNode(element);
+        currentLi.appendChild(currentLiText);
+        listContainer.appendChild(currentLi);
+    });
 })
 
 
-let container = document.getElementById('container');
