28f52614c86ba725e51aa6a089a296878bf40ad2
danielcb29 <danielc.web@gmail.com>
2017-12-16 13:20:13 -0500

    List movies with overview and image

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const apiKey = '6dc0d2605088c01254ffedbd444bc2e4';
+const imageUrl = 'http://image.tmdb.org/t/p/w300/';
 
 const getMovies = () => {
     return fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US`).then(movies => {
@@ -6,18 +7,62 @@ const getMovies = () => {
     });
 };
 
-let currentMovies = null;
-const listContainer = document.getElementById('movies-list');
 
-getMovies().then(response => {
-    currentMovies = response.results;
-    currentMovies.map(element => element.title).forEach(element => {
-        const currentLi = document.createElement('li');
-        currentLi.className = 'movie-title';
-        const currentLiText = document.createTextNode(element);
-        currentLi.appendChild(currentLiText);
-        listContainer.appendChild(currentLi);
+const loadMovies = () => {
+    const createMovieImage = (divImage, element) => {
+        const image = document.createElement('img');
+        image.setAttribute('src', `${imageUrl}${element.backdrop_path}`);
+        divImage.className = 'movie-image';
+        divImage.appendChild(image);
+    };
+
+    const createMovieContent = (divContent, element) => {
+        //Title
+        const divTitle = document.createElement('h2');
+        const titleText = document.createTextNode(element.title);
+        divTitle.appendChild(titleText);
+
+        //Overview
+        const divOverview = document.createElement('p');
+        divOverview.className = 'movie-overview';
+        const overviewText = document.createTextNode(element.overview);
+        divOverview.appendChild(overviewText);
+
+        divContent.className = 'movie-content';
+        //Add elements to divContent
+        divContent.appendChild(divTitle);
+        divContent.appendChild(divOverview);
+    };
+
+    let currentMovies = null;
+    const listContainer = document.getElementById('container');
+    
+    getMovies().then(response => {
+        listContainer.innerHTML = '';
+        currentMovies = response.results;
+        currentMovies.forEach(element => {
+            const div = document.createElement('div');
+            div.className = 'movie';
+    
+            const divContent = document.createElement('div');
+            const divImage = document.createElement('div');
+    
+            createMovieImage(divImage, element);
+            createMovieContent(divContent, element);
+    
+            //Add childs to div
+            div.appendChild(divImage);
+            div.appendChild(divContent);
+
+            // Add current div to container
+            listContainer.appendChild(div);
+        });
     });
-})
+};
 
 
+const main = () => {
+    loadMovies();
+}
+
+main();
